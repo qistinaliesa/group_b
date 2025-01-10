7677c2aeb97d2a986c3f14e2e18c0d0c5baa9d84
nurlyanana <148199929+nurlyanana@users.noreply.github.com>
2025-01-10 13:49:11 +0800

    Update details.blade.php

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('content')
-
+<style>
+    .filled-heart {
+        color: orange;
+    }
+</style>
 <main class="pt-90">
     <div class="mb-md-1 pb-md-3"></div>
     <section class="product-single container">
@@ -13,23 +17,23 @@
 
                   <div class="swiper-slide product-single__image-item">
                     <img loading="lazy" class="h-auto" src="{{asset('uploads/products')}}/{{$product->image}}" width="674" height="674" alt="" />
-                    <a data-fancybox="gallery" href="{{asset('uploads/products')}}/{{$product->image}}" data-bs-toggle="tooltip data-bs-placement="left" title="Zoom">
+                    <a data-fancybox="gallery" href="{{asset('uploads/products')}}/{{$product->image}}" data-bs-toggle="tooltip" data-bs-placement="left" title="Zoom">
                       <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_zoom" />
                       </svg>
                     </a>
                   </div>
 
-                  @foreach (explode(',',$product->images) as $gimg)
+                    @foreach(explode(',',$product->images) as $gimg)
                   <div class="swiper-slide product-single__image-item">
                     <img loading="lazy" class="h-auto" src="{{asset('uploads/products')}}/{{$gimg}}" width="674" height="674" alt="" />
-                    <a data-fancybox="gallery" href="{{asset('uploads/products')}}/{{$gimg}}" data-bs-toggle="tooltip" data-bs-placement="left" title="Zoom">
+                    <a data-fancybox="gallery" href="{{asset('uploads/products')}}/{{$gimg}}" data-bs-toggle="tooltip"  data-bs-placement="left" title="Zoom">
                       <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_zoom" />
                       </svg>
                     </a>
                   </div>
-                  @endforeach
+                    @endforeach
                 </div>
                 <div class="swiper-button-prev"><svg width="7" height="11" viewBox="0 0 7 11"
                     xmlns="http://www.w3.org/2000/svg">
@@ -45,9 +49,9 @@
               <div class="swiper-container">
                 <div class="swiper-wrapper">
                   <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto" src="{{asset('uploads/products/thumbnails')}}/{{$product->image}}" width="104" height="104" alt="" /></div>
-                  @foreach (explode(',',$product->images) as $gimg)
+                    @foreach(explode(',',$product->images) as $gimg)
                   <div class="swiper-slide product-single__image-item"><img loading="lazy" class="h-auto" src="{{asset('uploads/products/thumbnails')}}/{{$gimg}}" width="104" height="104" alt="" /></div>
-                  @endforeach
+                    @endforeach
                 </div>
               </div>
             </div>
@@ -97,31 +101,58 @@
           <div class="product-single__price">
             <span class="current-price">
                 @if($product->sale_price)
-                <s>${{$product->regular_price}} </s> ${{$product->sale_price}}
-                @else
-                ${{$product->regular_price}}
-                @endif
+                <s>RM{{$product->regular_price}}</s> RM{{$product->sale_price}}
+              @else
+                  RM{{$product->regular_price}}
+              @endif
             </span>
           </div>
           <div class="product-single__short-desc">
-            <p>{{$product->short_description}}</p>
+        <p>{{$product->short_description}}</p>
           </div>
-          <form name="addtocart-form" method="post">
+          @if(Cart::instance('cart')->content()->where('id', $product->id)->count() > 0)
+           <a href="{{route('cart.index')}}" class="btn btn-warning mb-3">Go to Cart</a>
+          @else
+          <form name="addtocart-form" method="post" action="{{route('cart.add')}}">
+            @csrf
             <div class="product-single__addtocart">
               <div class="qty-control position-relative">
                 <input type="number" name="quantity" value="1" min="1" class="qty-control__number text-center">
                 <div class="qty-control__reduce">-</div>
                 <div class="qty-control__increase">+</div>
               </div><!-- .qty-control -->
-              <button type="submit" class="btn btn-primary btn-addtocart js-open-aside" data-aside="cartDrawer">Add to
-                Cart</button>
+                <input type="hidden" name="id" value="{{$product->id}}" />
+                <input type="hidden" name="name" value="{{$product->name}}" />
+                <input type="hidden" name="price" value="{{$product->sale_price == '' ? $product->regular_price : $product->sale_price}}" />
+              <button type="submit" class="btn btn-primary btn-addtocart" data-aside="cartDrawer">Add to Cart</button>
             </div>
           </form>
+            @endif
           <div class="product-single__addtolinks">
-            <a href="#" class="menu-link menu-link_us-s add-to-wishlist"><svg width="16" height="16" viewBox="0 0 20 20"
+            @if(Cart::instance('wishlist')->content()->where('id', $product->id)->count() > 0)
+            <form method="POST" action="{{route('wishlist.items.remove', ['rowId'=>Cart::instance('wishlist')->content()->where('id', $product->id)->first()->rowId])}}" id="frm-remove-item">
+                @csrf
+                @method('DELETE')
+            <a href="javascript:void(0)" class="menu-link menu-link_us-s add-to-wishlist filled-heart" onclick="document.getElementById('frm-remove-item').submit()"><svg width="16" height="16" viewBox="0 0 20 20"
+                fill="none" xmlns="http://www.w3.org/2000/svg">
+                <use href="#icon_heart" />
+              </svg><span>Remove from Wishlist</span></a>
+            </form>
+              @else
+              <form method="POST" action="{{route('wishlist.add')}}" id="wishlist-form">
+              @csrf
+              <input type="hidden" name="id" value="{{$product->id}}" />
+              <input type="hidden" name="name" value="{{$product->name}}" />
+              <input type="hidden" name="price" value="{{$product->sale_price == '' ? $product->regular_price : $product->sale_price}}" />
+              <input type="hidden" name="quantity" value="1" />
+              <a href="javascript:void(0)" class="menu-link menu-link_us-s add-to-wishlist" onclick="document.getElementById('wishlist.form').submit()"><svg width="16" height="16" viewBox="0 0 20 20"
                 fill="none" xmlns="http://www.w3.org/2000/svg">
                 <use href="#icon_heart" />
               </svg><span>Add to Wishlist</span></a>
+             </form>
+              @endif
+
+
             <share-button class="share-button">
               <button class="menu-link menu-link_us-s to-share border-0 bg-transparent d-flex align-items-center">
                 <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -190,32 +221,7 @@
           <div class="tab-pane fade show active" id="tab-description" role="tabpanel"
             aria-labelledby="tab-description-tab">
             <div class="product-single__description">
-              {{$product->description}}
-            </div>
-          </div>
-          <div class="tab-pane fade" id="tab-additional-info" role="tabpanel" aria-labelledby="tab-additional-info-tab">
-            <div class="product-single__addtional-info">
-              <div class="item">
-                <label class="h6">Weight</label>
-                <span>1.25 kg</span>
-              </div>
-              <div class="item">
-                <label class="h6">Dimensions</label>
-                <span>90 x 60 x 90 cm</span>
-              </div>
-              <div class="item">
-                <label class="h6">Size</label>
-                <span>XS, S, M, L, XL</span>
-              </div>
-              <div class="item">
-                <label class="h6">Color</label>
-                <span>Black, Orange, White</span>
-              </div>
-              <div class="item">
-                <label class="h6">Storage</label>
-                <span>Relaxed fit shirt-style dress with a rugged</span>
-              </div>
-            </div>
+               {{$product->description}}
           </div>
           <div class="tab-pane fade" id="tab-reviews" role="tabpanel" aria-labelledby="tab-reviews-tab">
             <h2 class="product-single__reviews-title">Reviews</h2>
@@ -385,30 +391,39 @@
             }
           }'>
           <div class="swiper-wrapper">
-            @foreach ($rproducts as $rproduct)
+            @foreach($rproducts as $rproduct)
             <div class="swiper-slide product-card">
               <div class="pc__img-wrapper">
-                <a href="{{route('shop.product.details',['product_slug'=>$rproduct->slug])}}">
-                  <img loading="lazy" src="{{asset('uploads/products')}}/{{$rproduct->image}}" width="330" height="400" alt="{{$rproduct->name}}" width="330" height="400" alt="{{$rproduct_name}}" class="pc__img">
-                  @foreach (explode(",",$rproduct->image) as $gimg)
-                  <img loading="lazy" src="{{asset('uploads/products')}}/{{$gimg}}" width="330" height="400" alt="{{$rproduct->name}}" width="330" height="400" alt="Cropped Faux leather Jacket" class="pc__img pc__img-second">
-                  @endforeach
+                <a href="{{route('shop.product.details', ['product_slug'=>$rproduct->slug])}}">
+                  <img loading="lazy" src="{{asset('uploads/products')}}/{{$rproduct->image}}" width="330" height="400" alt="{{$rproduct->name}}" class="pc__img">
+                  @foreach(explode(",",$product->images) as $gimg)
+                  <img loading="lazy" src="{{asset('uploads/products')}}/{{$gimg}}" width="330" height="400" alt="{{$rproduct->name}}" class="pc__img pc__img-second">
+                    @endforeach
                 </a>
-                <button
-                  class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium js-add-cart js-open-aside"
-                  data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+                @if(Cart::instance('cart')->content()->where('id', $rproduct->id)->count() > 0)
+                <a href="{{route('cart.index')}}" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium btn-warning mb-3">Go to Cart</a>
+             @else
+             <form name="addtocart-form" method="post" action="{{route('cart.add')}}">
+                 @csrf
+                 <input type="hidden" name="id" value="{{$rproduct->id}}" />
+                 <input type="hidden" name="quantity" value="1" />
+                 <input type="hidden" name="name" value="{{$rproduct->name}}" />
+                 <input type="hidden" name="price" value="{{$rproduct->sale_price == '' ? $rproduct->regular_price : $rproduct->sale_price}}" />
+                 <button type="submit" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium" data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+             </form>
+             @endif
               </div>
 
               <div class="pc__info position-relative">
                 <p class="pc__category">{{$rproduct->category->name}}</p>
-                <h6 class="pc__title"><a href="{{route('shop.product.details',['product_slug'=>$product->slug])}}">{{$rproduct->name}}</a></h6>
+                <h6 class="pc__title"><a href="{{route('shop.product.details', ['product_slug'=>$rproduct->slug])}}">{{$rproduct->name}}</a></h6>
                 <div class="product-card__price d-flex">
                   <span class="money price">
                     @if($product->sale_price)
-                       <s>${{$product->regular_price}} </s> ${{$rproduct->sale_price}}
-                    @else
-                       ${{$product->regular_price}}
-                    @endif
+                    <s>RM{{$product->regular_price}}</s> RM{{$rproduct->sale_price}}
+                  @else
+                      RM{{$product->regular_price}}
+                  @endif
                   </span>
                 </div>
 
@@ -442,5 +457,5 @@
 
     </section><!-- /.products-carousel container -->
   </main>
-
 @endsection
+

676d2ee2bd5b690dfee26bda0044f5eb0d4b5473
Hafiz <hafizfirdaus2111@gmail.com>
2025-01-12 02:08:45 +0800

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+
 
 use Illuminate\Http\Request;
 
@@ -10,4 +13,25 @@ class UserController extends Controller
     {
         return view('user.index');
     }
+
+    public function orders()
+    {
+        $orders = Order::where('user_id',Auth::user()->id)->orderBy('created_at','Desc')->paginate(10);
+        return view('user.orders',compact('orders'));
+    }
+
+    public function order_details($order_id)
+    {
+        $order = Order::where('user_id',Auth::user()->id)->where('id',$order_id)->first();
+        if($order)
+        {
+        $order = Order::where('user_id',Auth::user()->id)->where('id',$order_id)->first();
+        $orderItems = OrderItem::where('order_id',$order_id)->orderBy('id')->paginate(12);
+        $transaction = Transaction::where('order_id',$order_id)->first();
+        return view('user_order-details',compact('order','orderItems','transaction'));
+            }
+            else{
+                return redirect()->route('login');
+            }
+        }
 }

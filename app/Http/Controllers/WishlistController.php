5d11ec43d423073aba20e23fcddd4ebc9f1d59dc
hajaraisyah <nikaisyah330@gmail.com>
2025-01-11 14:38:35 +0800

    Update WishlistController.php

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -2,32 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\WishlistController;
+use App\Http\Controllers\AuthAdmin;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Surfsidemedia\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
 
 class WishlistController extends Controller
 {
     public function index()
     {
-       $items = Cart::instance('wishlist')->content();
-         return view('wishlist', compact('items'));
+        $items = Cart::instance('wishlist')->content();
+        return view('wishlist',compact('items'));
     }
 
     public function add_to_wishlist(Request $request)
     {
-       Cart::instance('wishlist')->add($request->id, $request->name, $request->quantity, $request->price)->associate('App\Models\Product');
-       return redirect()->back();
+        Cart::instance('wishlist')->add($request->id,$request->name,$request->quantity,$request->price)->associate('App\Models\Product');
+        return redirect()->back();
     }
-
     public function remove_item($rowId)
     {
         Cart::instance('wishlist')->remove($rowId);
         return redirect()->back();
     }
-
     public function empty_wishlist()
     {
         Cart::instance('wishlist')->destroy();
+        return redirect()->back();
+    }
+    public function move_to_cart($rowId)
+    {
+        $item = Cart::instance('wishlist')->get($rowId);
+        Cart::instance('wishlist')->remove($rowId);
+        Cart::instance('cart')->add($item->id,$item->name,$item->qty,$item->price)->associate('App\Models\Product');
         return redirect()->back();
     }
 }

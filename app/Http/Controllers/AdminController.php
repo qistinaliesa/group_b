2399fffbd3c6b7af5b36926a460e70c0e6cf509a
Hafiz <hafizfirdaus2111@gmail.com>
2025-01-13 01:08:14 +0800

    m

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Transaction;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -447,7 +449,7 @@ public function product_delete($id)
 }
 public function orders()
     {
-        $orders = Order::orderBy('create_at','DESC')->paginate(12);
+        $orders = Order::orderBy('created_at','DESC')->paginate(12);
         return view('admin.orders',compact('orders'));
     }
 
@@ -455,8 +457,10 @@ public function orders()
         $order = Order::find($order_id);
           $orderitems = OrderItem::where('order_id',$order_id)->orderBy('id')->paginate(12);
           $transaction = Transaction::where('order_id',$order_id)->first();
+          dd($transaction);
           return view("admin.order-details",compact('order','orderitems','transaction'));
     }
+
     public function update_order_status (Request $request)
     {
         $order = Order::find($request->order_id);

3d179acf0d39f1d6fbc77c1b23861533c439e269
Hafiz <hafizfirdaus2111@gmail.com>
2025-01-13 02:37:04 +0800

    a

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -61,7 +61,7 @@ class ShopController extends Controller
     public function product_details($product_slug)
     {
         $product = Product::where('slug', $product_slug)->first();
-        $rproduct = Product::where('slug', '<>', $product_slug)->get()->take(8);
-        return view('details', compact('product', 'rproduct'));
+        $rproducts = Product::where('slug', '<>', $product_slug)->get()->take(8);
+        return view('details', compact('product', 'rproducts'));
     }
 }
